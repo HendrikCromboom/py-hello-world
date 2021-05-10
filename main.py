288d2c546d1af531d04ad8faf44358ead67647fa
HendrikCromboom <68901906+HendrikCromboom@users.noreply.github.com>
2021-05-10 23:44:42 +0200

    basic if else, input and type conversion

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 message = "Hello" + " " + "World"
-is_true = True
+is_true = False
 if is_true:
     print(message)
+name = input("What is your name? ")
+print("Hello " + name + "!")
+age = input("How old are you? ")
+if int(age) < 100:
+    print("Pretty young I see!")
+else:
+    print("Pretty wise I see!")
